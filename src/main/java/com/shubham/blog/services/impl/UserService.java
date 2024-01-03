0cfeed9ac77af707b03cef663ca2d4d260bf7e84
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-01-03 16:25:08 +0530

    feat:Adding User Entity

---
+++ src/main/java/com/shubham/blog/services/impl/UserService.java
@@ -0,0 +1,39 @@
+package com.shubham.blog.services.impl;
+
+import java.util.List;
+
+import com.shubham.blog.payloads.UserDto;
+
+public class UserService implements com.shubham.blog.services.UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto updateUser(UserDto user, Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserDto getUserById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUser(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
